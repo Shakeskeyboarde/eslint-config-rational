b19f38ee20409177cd61ba6b1afeb32f3c7bc181
Chris Ackerman <chris@topher.land>
2022-04-19 23:49:55 -0700

    Relaxed some naming conventions.

--- test/typescript.ts
+++ test/typescript.ts
@@ -1,1 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const props = '';
+const prop = '';
+const args = '';
+const str = '';
+// const acc = '';
+// const dev = '';
+// const prod = '';
+// const err = '';
+
 export {};
