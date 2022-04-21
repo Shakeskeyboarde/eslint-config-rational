ed9c330bf6c4de4b082760c362c9247b4b035678
Chris Ackerman <chris@topher.land>
2022-04-20 21:11:09 -0700

    Added some additional typescript rules

--- typescript.js
+++ typescript.js
@@ -7,6 +7,7 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   rules: {
+    '@typescript-eslint/array-type': 'warn',
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/consistent-type-definitions': ['warn', 'type'],
     '@typescript-eslint/consistent-type-imports': 'warn',
@@ -14,6 +15,9 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-floating-promises': 'warn',
     '@typescript-eslint/no-shadow': 'warn',
+    '@typescript-eslint/prefer-for-of': 'warn',
+    '@typescript-eslint/prefer-reduce-type-parameter': 'warn',
+    '@typescript-eslint/prefer-string-starts-ends-with': 'warn',
     '@typescript-eslint/sort-type-union-intersection-members': 'warn',
     'canonical/prefer-inline-type-import': 'warn',
     'functional/prefer-readonly-type': ['warn', { allowLocalMutation: true, allowMutableReturnType: true }],
