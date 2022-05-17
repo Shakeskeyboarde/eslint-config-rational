98bcc792a6d37fc9d6a1b0746be6f88483fa14b4
Chris Ackerman <chris@topher.land>
2022-05-17 05:53:45 -0700

    Disabled react/prop-types rule

--- react.js
+++ react.js
@@ -1,5 +1,8 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ['plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:react-hooks/recommended'],
+  rules: {
+    'react/prop-types': 'off',
+  },
   settings: { react: { version: 'detect' } },
 };
