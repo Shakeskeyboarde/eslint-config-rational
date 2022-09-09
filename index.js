d48862815b72203ed2ed1b80451e79388a23da14
Chris Ackerman <chris@topher.land>
2022-09-08 17:08:10 -0700

    Added finalize to the list of prefixes that are ignored for abbreviations.

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ module.exports = {
       'warn',
       {
         checkFilenames: false,
-        ignore: ['^_*(?:create|get|set|has|remove|on|is)?[A-Z]'],
+        ignore: ['^_*(?:create|get|set|has|remove|on|is|finalize)?[A-Z]'],
         replacements: {
           acc: { accumulator: false, result: true },
           accumulator: { result: true },
