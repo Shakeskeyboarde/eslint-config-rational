21a39804bea939e17ed72be05ea6d6a1c1c687a4
Chris Ackerman <chris@topher.land>
2023-07-07 11:06:58 -0700

    fix: Don't use compat loader for prettier plugin/config.

--- src/prettier.ts
+++ src/prettier.ts
@@ -1,19 +1,21 @@
-import compat from './utils/compat.js';
+import type { ESLint } from 'eslint';
+import prettierConfig from 'eslint-config-prettier';
+import prettier from 'eslint-plugin-prettier';
+
 import { configFactory } from './utils/config-factory.js';
 
 export default configFactory<{
   readonly files: readonly string[];
 }>(({ files }) => {
   return [
-    // TODO: Update this to flat configuration compatible version when released.
-    ...compat({
-      files,
-      extends: ['plugin:prettier/recommended'],
-    }),
-
     {
       files,
+      plugins: {
+        prettier,
+      },
       rules: {
+        ...prettierConfig.rules,
+        ...(prettier.configs?.recommended as ESLint.ConfigData).rules,
         'prettier/prettier': 'warn',
       },
     },
